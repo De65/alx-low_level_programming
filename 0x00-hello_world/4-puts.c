84f6bda8f556baee41c49cc9f1ccfc342ea47db2
De65 <kyalodiana6@gmail.com>
2022-06-30 13:35:05 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
-#!/bin/bash
-# include <stdio.h>
-  int main (void)
-       {
-	       puts ("\" programming is fun") ;
-	       return (0);
-       }
+#include <stdio.h>
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
