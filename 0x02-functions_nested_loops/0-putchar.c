4878b2602beab56635fa37bc1fe85073efa89aa6
De65 <kyalodiana6@gmail.com>
2022-07-04 22:09:50 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ * Return: Always 0.
+*/
+
+int main(void)
+
+{
+
+char holberton[] = "Holberton";
+
+int i;
+
+for (i = 0; holberton[i] != '\0'; i++)
+
+{
+
+_putchar(holberton[i]);
+
+}
+
+_putchar('\n');
+
+return (0);
+
+}
