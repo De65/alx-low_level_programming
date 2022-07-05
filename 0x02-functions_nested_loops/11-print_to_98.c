e554e2588607165a295efe65af3914aa39ff84d5
De65 <kyalodiana6@gmail.com>
2022-07-05 15:50:29 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,69 +9,36 @@
 void print_to_98(int n)
 
 {
-
-	int upper = 98;
-
-
-
-	if (n > upper)
-
-	{
-
-		while (n >= upper)
-
-		{
-
-			if (n != upper)
-
-			{
-
-				printf("%d%s", n, ", ");
-
-			}
-
-			else
-
-			{
-
-				printf("%d", n);
-
-			}
-
-			n--;
-
-		}
-
-	}
-
-	else
-
-	{
-
-		while (n <= upper)
-
-		{
-
-			if (n != upper)
-
-			{
-
-				printf("%d%s", n, ", ");
-
-			}
-
-			else
-
-			{
-
-				printf("%d", n);
-
-			}
-
-			n++;
-
-		}
-
+int upper = 98;
+if (n > upper)
+{
+while (n >= upper)
+{
+if (n != upper)
+{
+printf("%d%s", n, ", ");
+}
+else
+{
+printf("%d", n);
+}
+n--;
+}
+}
+else
+{
+while (n <= upper)
+{
+if (n != upper)
+{
+printf("%d%s", n, ", ");
+}
+else
+{
+printf("%d", n);
+}
+n++;
+}
 }
 putchar(10);
 }
