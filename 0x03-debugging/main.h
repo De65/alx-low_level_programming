9521c4e42c10455b478d8bdf36dfa7947c869a4f
De65 <kyalodiana6@gmail.com>
2022-07-06 13:02:02 -0700

    task 0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,11 +1,7 @@
 #ifndef HEADER_FILE
-
 #define HEADER_FILE
 
-
-
 int _putchar(char c);
-
 
 
 void positive_or_negative(int i);
@@ -15,7 +11,5 @@ int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 
 void print_remaining_days(int month, int day, int year);
-
-
 
 #endif
