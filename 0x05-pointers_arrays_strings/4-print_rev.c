7e17c9c73e3fca4d8e0bf49b5c6c354ef3227734
De65 <kyalodiana6@gmail.com>
2022-07-11 11:07:29 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
- * print_rev -function
+ * print_rev - function
  *
  * @s: pointer of parameter
  */
 void print_rev(char *s)
 {
-int length = 0;
+	int length = 0;
 
-while (s[lenght] != '\0 ')
-length++;
-while (lenght > 0)
-_putchar(10);
+	while (s[length] != '\0')
+		length++;
+	while (length > 0)
+		_putchar(s[--length]);
+	_putchar(10);
 }
