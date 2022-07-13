9a0b933fe3d91e17f2d3d5bbac49aa0adaff2067
De65 <kyalodiana6@gmail.com>
2022-07-13 11:44:08 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strcmp - reverse an array
+ * @s1: array of integers
+ * @s2: integer
+ * Description: function is similar to the strncpy function,
+ * Return: pointer to the resulting string dest
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0 ')
+	{
+		if (s1[i] != s2[i])
+			break;
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
