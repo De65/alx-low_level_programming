1ba321f3006fe3a0aa411407bdc41bc60af8e9e9
De65 <kyalodiana6@gmail.com>
2022-07-13 09:04:04 -0700

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+/**
+ * _strncat - function is similar to the _strcat function
+ * @dest: string destination
+ * @src: string destination
+ * @n: number of characters
+ * Description: function is similar to the _strcat function,
+ * Return: pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+char *ptr;
+int longitud = 0;
+
+while (dest[longitud] != '\0 ')
+longitud++;
+
+ptr = dest + longitud;
+
+while (*src != '\0 ')
+*ptr++ = *src++;
+
+*ptr = '\0 ';
+return (dest);
+}
