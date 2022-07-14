9630aadddac156344e1f0ea9b2f2cec138f23cd8
De65 <kyalodiana6@gmail.com>
2022-07-14 10:08:22 -0700

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,19 +3,20 @@
 /**
  * string_toupper - convert lowercase to uppercase
  * @s: array of char
- * Description: function is similar to lowercase function,
+ *
+ * Description:  function is similar to lowerCase function,
  * Return: char string
  */
 char *string_toupper(char *s)
 {
-	int i;
+int i;
 
-	for (i = 0; s[i] != '\0 '; i++)
-	{
-		if (s[i] >= 'a' && s[i] <= 'z')
-		{
-			s[i] = s[i] - 32;
-		}
-	}
-	return (s);
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] >= 'a' && s[i] <= 'z')
+{
+s[i] = s[i] - 32;
+}
+}
+return (s);
 }
