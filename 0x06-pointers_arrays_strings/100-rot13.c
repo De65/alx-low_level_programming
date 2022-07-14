ebca186397a801f131957c4776da621f0e94ac7b
De65 <kyalodiana6@gmail.com>
2022-07-14 10:53:43 -0700

    task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * rot13 - encodes a string using rot13.
+ * @s: array of char
+ * Description:  function that encodes a string using rot13.
+ * Return: char string
+ */
+char *rot13(char *s)
+{
+char abc[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+int i, j;
+
+for (i = 0; s[i] != '\0' ; i++)
+{
+for (j = 0; abc[j] != '\0' ; j++)
+{
+if (s[i] == abc[j])
+{
+s[i] = rot[j];
+break;
+}
+}
+}
+return (s);
+}
