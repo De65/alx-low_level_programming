d74f659caa41da46566b3ba1c351600f0fa26f98
De65 <kyalodiana6@gmail.com>
2022-07-14 10:19:20 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * cap_string - convert lowercase to uppercase capitalize
+ * @s: array of char
+ *
+ * Description:  function is similar to lowerCase function,
+ * Return: char string
+ */
+char *cap_string(char *s)
+{
+int i;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
+s[i] = s[i] - 32;
+
+if ((s[i] >= 'a' && s[i] <= 'z') &&
+(s[i - 1] == 32 || s[i - 1] == '\t' || s[i - 1] == '\n' || s[i - 1] == ','
+|| s[i - 1] == ';' || s[i - 1] == '.'  || s[i - 1] == '!' || s[i - 1] == '?'
+|| s[i - 1] == '"' || s[i - 1] == '('  || s[i - 1] == ')' || s[i - 1] == '{'
+|| s[i - 1] == '}'))
+
+s[i] = s[i] - 32;
+}
+return (s);
+}
