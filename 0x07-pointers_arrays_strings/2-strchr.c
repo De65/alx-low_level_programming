708e95f7a23dd9b43ff2802c282fc6f672a565ce
De65 <kyalodiana6@gmail.com>
2022-07-18 11:19:05 -0700

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,22 +3,23 @@
 * _strchr - function that locates a character in a string.
 * @s: array of char
 * @c: char
-* Description: function that locates a character in a strng.
+* Description:  function that locates a character in a string.
 * Return: char string
-*/
+ */
 char *_strchr(char *s, char c)
 {
-for ( ; *s != '\0 '; s++)
+
+
+for ( ; *s != '\0' ; s++)
 {
 if (*s == c)
 {
 return (s);
 }
-if (*s == c)
-{
-return (s);
-}
-}
-return (NULL);
 }
 
+if (*s == c)
+return (s);
+
+return (NULL);
+}
