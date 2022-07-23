24cefd5c7d8f1dfc819a8fcbc396b4b79de968a5
De65 <kyalodiana6@gmail.com>
2022-07-23 11:50:36 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check the code
+ * @argc : argc contains the number of arguments passed to the program
+ * @argv : A vector is a one-dimensional array
+ * Return: Always 0.
+ */
+int main(int argc, _attribute_((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
