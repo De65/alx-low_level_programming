07d67a2c7bf5122feaab083e53af64c763209511
De65 <kyalodiana6@gmail.com>
2022-08-04 07:30:05 -0700

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,46 +1,84 @@
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
 /**
- * print_all - prints anything.
- * @format: list of types of arguments passed to the function
+ * print_char - entry va_list like type
+ * @c: get va_list
+ */
+void print_char(va_list c)
+{
+	printf("%c", va_arg(c, int));
+}
+/**
+ * print_int - entry va_list like type
+ * @i: get va_list
+ */
+void print_int(va_list i)
+{
+	printf("%i", va_arg(i, int));
+}
+/**
+ * print_float - entry va_list like type
+ * @f: get va_list
+ */
+void print_float(va_list f)
+{
+	printf("%f", va_arg(f, double));
+}
+/**
+ * print_string - entry va_list like type
+ * @s: get va_list
+ */
+void print_string(va_list s)
+{
+	char *str = va_arg(s, char *);
+
+	if (str != NULL)
+	{
+		printf("%s", str);
+		return;
+	}
+	printf("(nil)");
+}
+/**
+ * print_all - entry arguments
+ * @format: get quantity aarguments
  */
 void print_all(const char * const format, ...)
 {
-char *cad;
-int cont = 0, other;
-va_list parameters;
+	format_t type[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}
+	};
 
-va_start(parameters, format);
-while (*(format + cont) != '\0' && format != NULL)
-{
-switch (format[cont])
-{
-case 'c':
-other = 0;
-printf("%c", va_arg(parameters, int));
-break;
-case 'i':
-other = 0;
-printf("%d", va_arg(parameters, int));
-break;
-case 'f':
-other = 0;
-printf("%f", va_arg(parameters, double));
-break;
-case 's':
-other = 0;
-cad = va_arg(parameters, char *);
-if (cad == NULL)
-cad = "(nil)";
-printf("%s", cad);
-break;
-default:
-other = 1;
-break;
-}
-if (format[cont + 1] != '\0' && other != 1)
-printf(", ");
-cont++;
-}
-va_end(parameters);
-printf("\n");
+	va_list args;
+	int i, j;
+	char *sp = "";
+
+	va_start(args, format);
+	i = 0;
+	while (format && format[i])
+	{
+		j = 0;
+		while (j < 4)
+		{
+
+			if (type[j].t[0] == format[i])
+			{
+				printf("%s", sp);
+				type[j].f(args);
+				sp = ", ";
+				break;
+
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(args);
 }
