2964f9d11b8720d9b32bfcaf7c90528210ff21f8
De65 <kyalodiana6@gmail.com>
2022-08-15 10:38:58 -0700

    task 3

--- 0x13-more_singly_linked_lists/3-add_nodient_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodient_end.c
@@ -1,32 +1,34 @@
 #include "lists.h"
-
 /**
- * add_nodeint_end - adds a new node at the end
- * @head: head of list
- * @n: int
- * Description: adds a new node at the beginning of a list_t list.
- * Return: address of the new element, or NULL if it failed
+ * add_nodeint_end - Entry Point
+ * @head: head
+ * @n: value
+ * Return: 0
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-listint_t *node = NULL;
-listint_t *actual = *head;
+	listint_t *new;
+	listint_t *temp;
 
-node = malloc(sizeof(listint_t));
-if (node == NULL)
-return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
 
-node->n = n;
-node->next = NULL;
+	new->n = n;
+	new->next = NULL;
 
-if ((*head) == NULL)
-(*head) = node;
-else
-{
-while (actual->next != NULL)
-actual = actual->next;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
 
-actual->next = node;
-}
-return (*head);
+	temp = *head;
+
+	while (temp->next != NULL)
+		temp = temp->next;
+
+	temp->next = new;
+
+	return (new);
 }
