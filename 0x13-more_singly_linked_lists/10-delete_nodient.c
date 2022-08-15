49cbe0a7f6ed81cbc8852554d6e7f83ed38b67b0
De65 <kyalodiana6@gmail.com>
2022-08-15 11:10:34 -0700

    task 10

--- 0x13-more_singly_linked_lists/10-delete_nodient.c
+++ 0x13-more_singly_linked_lists/10-delete_nodient.c
@@ -1,42 +1,36 @@
 #include "lists.h"
 /**
- * delete_nodeint_at_index - Entry Point
- * @head: head
- * @index: index
- * Return: 0
+ * delete_nodeint_at_index - Delete a node
+ * @head: head of list
+ * @index: index of node
+ * Description: deletes the node at index index
+ * Return: int
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	unsigned int count = 0;
+listint_t *next, *temp;
+unsigned int cont = 0;
 
-	listint_t *copy;
-	listint_t *temp;
+if (*head == NULL)
+return (-1);
 
-	if (*head == NULL)
-		return (-1);
+temp = *head;
+if (index == 0)
+{
+*head = temp->next;
+free(temp);
+return (1);
+}
 
-	copy = *head;
+for ( ; temp != NULL && cont < index - 1; cont++)
+temp = temp->next;
 
-	if (index == 0)
-	{
-		temp = copy->next;
-		free(copy);
-		*head = temp;
-		return (1);
-	}
+if (temp == NULL || temp->next == NULL)
+return (-1);
 
-	while (copy != NULL)
-	{
-		if (count + 1 == index)
-		{
-			temp = copy->next;
-			copy->next = temp->next;
-			free(temp);
-			return (1);
-		}
-		if (copy->next != NULL)
-			copy = copy->next;
-		count++;
-	}
-	return (-1);
+next = temp->next->next;
+free(temp->next);
+temp->next = next;
+return (1);
+
 }
