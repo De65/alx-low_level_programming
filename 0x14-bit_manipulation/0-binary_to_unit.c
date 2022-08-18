3074048b205918b8205f88ca3e2c8ddaf0434858
De65 <kyalodiana6@gmail.com>
2022-08-18 11:13:54 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_unit.c
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -1,32 +1,49 @@
-#include "main.h"
+#include "holberton.h"
+int _pow(int x, int y);
+
 /**
- * binary_to_uint - Entry Point
- * @b: const char
- * Return: 0
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: string of 0 and 1
+ * Description: Print elements of a list.
+ * Return: unsigned int
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int res = 0;
-	int base = 1, i = 0;
+int i = 0, len = 0;
+unsigned int number = 0;
 
-	if (b == NULL)
-		return (0);
+if (b == NULL)
+return (0);
 
-	while (b[i + 1])
-	{
-		if (b[i] != '0' && b[i] != '1')
-			return (0);
-		i++;
-	}
+for ( ; b[i] != '\0'; i++)
+len++;
 
-	while (i >= 0)
-	{
-		res += ((b[i] - '0') * base);
-		base *= 2;
-		i--;
-	}
+for (i = 0 ; b[i] != '\0'; i++)
+{
+if (b[i] == '1')
+number = _pow(2, ((len - 1) - i)) + number;
+else if (b[i] != '0')
+return (0);
+}
+return (number);
+}
 
+/**
+ * _pow -  Pows a numbe to the x potence
+ * @x: base
+ * @y: exponent
+ * Return: The value
+ */
+int _pow(int x, int y)
+{
+int i, r = x;
 
-	return (res);
+if (y == 0)
+return (1);
 
+for (i = 1; i < y; i++)
+r = r * (x + 0);
+
+return (r);
 }
