376b2ca2df23dd332b43edf55b5f631529fd631b
De65 <kyalodiana6@gmail.com>
2022-08-19 13:01:40 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_unit.c
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -1,47 +1,48 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "holberton.h"
-
+#include "main.h"
+int _pow(int x, int y);
 /**
-  * binary_to_uint - Converts a binary number to an unsigned int
-  * @b: The binary string to converts
-  *
-  * Return: The positive number converted from a binary
-  */
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: string of 0 and 1
+ * Description: Print elements of a list.
+ * Return: unsigned int
+ */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int len = 0, count = 0, sum = 0;
+int i = 0, len = 0;
+unsigned int number = 0;
 
-	if (b == NULL)
-		return (0);
+if (b == NULL)
+return (0);
 
-	len = _strlen(b);
-	while (len--)
-	{
-		if (b[len] != 48 && b[len] != 49)
-			return (0);
+for ( ; b[i] != '\0'; i++)
+len++;
 
-		if (b[len] == 49)
-			sum += 1 << count;
-
-		count++;
-	}
-
-	return (sum);
+for (i = 0 ; b[i] != '\0'; i++)
+{
+if (b[i] == '1')
+number = _pow(2, ((len - 1) - i)) + number;
+else if (b[i] != '0')
+return (0);
+}
+return (number);
 }
 
 /**
-  * _strlen - Returns the length of a string
-  * @s: String to count
-  *
-  * Return: String length
-  */
-int _strlen(const char *s)
+ * _pow -  Pows a numbe to the x potence
+ * @x: base
+ * @y: exponent
+ * Return: The value
+ */
+int _pow(int x, int y)
 {
-	int c = 0;
+int i, r = x;
 
-	while (s[c])
-		c++;
+if (y == 0)
+return (1);
 
-	return (c);
+for (i = 1; i < y; i++)
+r = r * (x + 0);
+
+return (r);
 }
